83858c553d7e2eee2720b38a4e11b3a6ed9914c5
km-isource <kimsykimanimburu@gmail.com>
2025-03-05 13:22:40 +0300

    Product Variations

--- Ecommerce/admin.py
+++ Ecommerce/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Variation
 
 # Register your models here.
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'product_slug': ('product_name',)}
     list_display = ('product_name', 'category', 'modified_date', 'price', 'stock', 'is_available')
+    list_editable = ('is_available', 'stock')
+    
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
 
 admin.site.register(Products, ProductsAdmin)
+admin.site.register(Variation, VariationAdmin)
