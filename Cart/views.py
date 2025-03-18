7651e399aaf4c857126c0f04f6f16c25afe0981a
km-isource <kimsykimanimburu@gmail.com>
2025-03-18 13:01:52 +0300

    Removed product variations in views and templates

--- Cart/views.py
+++ Cart/views.py
@@ -12,17 +12,17 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Products.objects.get(id=product_id) #get the product
-    product_variation = []
-    if request.method == 'POST':
-        for item in request.POST:
-            key = item
-            value = request.POST[key]
+    # product_variation = []
+    # if request.method == 'POST':
+    #     for item in request.POST:
+    #         key = item
+    #         value = request.POST[key]
             
-            try:
-                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
-                product_variation.append(variation)
-            except:
-                pass       
+    #         try:
+    #             variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+    #             product_variation.append(variation)
+    #         except:
+    #             pass       
         
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
@@ -34,39 +34,52 @@ def add_cart(request, product_id):
     
     is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
     if is_cart_item_exists:
-        cart_item = CartItem.objects.filter(product=product, cart=cart)
-        ex_var_list = []
-        for item in cart_item:
-            existing_variation = item.variations.all()
-            ex_var_list.append(existing_variation)
-            
-        print(ex_var_list)
-        
-        if product_variation in ex_var_list:
-            return HttpResponse('Product already in cart')
-        else:
-            return HttpResponse('False')
-        
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-        # cart_item.quantity += 1  #cart_item.quantity = existing quantity + 1
-        # cart_item.save()
-        
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
     else:
         cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1,
-            cart = cart,
+            product=product,
+            quantity=1,
+            cart=cart,
         )
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
         cart_item.save()
-    
+
     return redirect('cart')
+    # if is_cart_item_exists:
+    #     cart_item = CartItem.objects.filter(product=product, cart=cart)
+    #     ex_var_list = []
+    #     for item in cart_item:
+    #         existing_variation = item.variations.all()
+    #         ex_var_list.append(existing_variation)
+            
+    #     print(ex_var_list)
+        
+    #     if product_variation in ex_var_list:
+    #         return HttpResponse('Product already in cart')
+    #     else:
+    #         return HttpResponse('False')
+        
+    #     if len(product_variation) > 0:
+    #         cart_item.variations.clear()
+    #         for item in product_variation:
+    #             cart_item.variations.add(item)
+    #     # cart_item.quantity += 1  #cart_item.quantity = existing quantity + 1
+    #     # cart_item.save()
+        
+    # else:
+    #     cart_item = CartItem.objects.create(
+    #         product = product,
+    #         quantity = 1,
+    #         cart = cart,
+    #     )
+    #     if len(product_variation) > 0:
+    #         cart_item.variations.clear()
+    #         for item in product_variation:
+    #             cart_item.variations.add(item)
+    #     cart_item.save()
+    
+    # return redirect('cart')
 
 def remove_cart(request, product_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
